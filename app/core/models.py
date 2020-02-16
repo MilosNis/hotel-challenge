be597f5f67dcbdd6fec4ec3d42e1f56002f6dd67
Milos Sehic <milos.sehic@miticon.com>
2020-02-16 14:28:02 +0100

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -20,6 +20,7 @@ class UserManager(BaseUserManager):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
@@ -30,6 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     display_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
